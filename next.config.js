81cb66884637f3b30a1787cf17db80d9e0b5d2f2
Tarun-hun-yar <tarunthakur283@gmail.com>
2025-10-08 21:05:58 +0530

    debugging

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*", // All requests to /api/* will be proxied
+        destination: "https://chat-shat-backend.onrender.com/api/:path*",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
